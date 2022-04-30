1ea78bd0b11552823a6bd669b12103df9d9dca20
kooooichi24 <ex.kouichi1224@gmail.com>
2022-04-30 18:16:11 +0900

    fix: package.json

--- backend/src/interface/repository/sample/SampleRepository.ts
+++ backend/src/interface/repository/sample/SampleRepository.ts
@@ -1,8 +1,8 @@
-import { ISampleRepository } from 'src/application/repository/ISampleRepository';
-import { Sample } from 'src/domain/sample/Sample';
+import { ISampleRepository } from "src/application/repository/ISampleRepository";
+import { Sample } from "src/domain/sample/Sample";
 
 export class SampleRepository implements ISampleRepository {
-  save(sample: Sample): void {
+  save(_: Sample): void {
     // throw new Error('Method not implemented.');
   }
 }
