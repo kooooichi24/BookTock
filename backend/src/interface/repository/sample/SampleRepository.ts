47bbb26edd2cad57cbf96065bb81d94947af40a2
kooooichi24 <ex.kouichi1224@gmail.com>
2022-05-01 16:37:50 +0900

    delete: prisma

--- backend/src/interface/repository/sample/SampleRepository.ts
+++ backend/src/interface/repository/sample/SampleRepository.ts
@@ -1,19 +1,18 @@
-import { PrismaClient } from "@prisma/client";
 import { ISampleRepository } from "src/application/repository/ISampleRepository";
 import { Sample } from "src/domain/sample/Sample";
 
 export class SampleRepository implements ISampleRepository {
-  private prisma: PrismaClient;
+  // private prisma: PrismaClient;
 
   constructor() {
-    this.prisma = new PrismaClient();
+    // this.prisma = new PrismaClient();
   }
 
   async save(sample: Sample): Promise<void> {
-    await this.prisma.sample.create({
-      data: {
-        name: sample.name,
-      },
-    });
+    // await this.prisma.sample.create({
+    //   data: {
+    //     name: sample.name,
+    //   },
+    // });
   }
 }
